8c8ae9a54ebe455fb685e31c4976d3729a14a679
Enrico Indiogine <hindiogine@gmail.com>
2016-03-04 21:40:15 -0700

    ultimo del giorno

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # MasterWide
-SimStudent test analysis
+SimStudent test analysis of results.
+
+-1 - incorrect answer
+ 0 - does not know the answer
+ 1 - correct answer
+
+http://stats.stackexchange.com/questions/7948/when-is-it-ok-to-remove-the-intercept-in-lm
+
+Removing the intercept is a different model, but there are plenty of examples where it is legitimate. Answers so far have already discussed in detail the example where the true intercept is 0. I will focus on a few examples where we may be interested in an atypical model parametrization.
--- TAMU.R
+++ TAMU.R
@@ -1,3 +1,57 @@
-# import csv
-setwd("/home/henk/Code/")
-my_data <- read.csv(file="MasterWide_StudyVI_TestScores_Sample.csv")
+require(xlsx)
+rm(list=ls(all=TRUE))
+
+filename <- "MasterWide_StudyVI_TestScores_Sample.xlsx"
+setwd("/home/henk/Code/MasterWide")
+
+temp <- read.xlsx(file = filename, sheetName = "MasterWide_StudyVI_TestScores")
+
+# Need to remove NAs.
+raw_data <- na.omit(temp)
+rm(temp)
+
+data_pre <- data.frame(Condition = raw_data$Condition.Name,
+                       School    = raw_data$School,
+                       Class     = raw_data$Class,
+                       LT_problem1_option1 = raw_data$Pre.Test.LT_problem1_option1,
+                       LT_problem1_option2 = raw_data$Pre.Test.LT_problem1_option2,
+                       LT_problem1_option3 = raw_data$Pre.Test.LT_problem1_option3,
+                       LT_problem1_option4 = raw_data$Pre.Test.LT_problem1_option4,
+                       LT_problem1_option5 = raw_data$Pre.Test.LT_problem1_option5,
+                       LT_problem1_option6 = raw_data$Pre.Test.LT_problem1_option6,
+                       LT_problem2_option1 = raw_data$Pre.Test.LT_problem2_option1,
+                       LT_problem2_option2 = raw_data$Pre.Test.LT_problem2_option2,
+                       LT_problem2_option3 = raw_data$Pre.Test.LT_problem2_option3,
+                       LT_problem2_option4 = raw_data$Pre.Test.LT_problem2_option4,
+                       LT_problem2_option5 = raw_data$Pre.Test.LT_problem2_option5,
+                       LT_problem2_option6 = raw_data$Pre.Test.LT_problem2_option6,
+                       eq_problem1_box     = raw_data$Pre.Test.eq_problem1_box,
+                       eq_problem2_box     = raw_data$Pre.Test.eq_problem2_box,
+                       eq_problem3_box     = raw_data$Pre.Test.eq_problem3_box,
+                       eq_problem4_box     = raw_data$Pre.Test.eq_problem4_box,
+                       eq_problem5_box     = raw_data$Pre.Test.eq_problem5_box,
+                       eq_problem6_box     = raw_data$Pre.Test.eq_problem6_box,
+                       eq_problem7_box     = raw_data$Pre.Test.eq_problem7_box)
+
+data_post <- data.frame(Condition = raw_data$Condition.Name,
+                        School    = raw_data$School,
+                        Class     = raw_data$Class,
+                        LT_problem1_option1 = raw_data$Post.Test.LT_problem1_option1,
+                        LT_problem1_option2 = raw_data$Post.Test.LT_problem1_option2,
+                        LT_problem1_option3 = raw_data$Post.Test.LT_problem1_option3,
+                        LT_problem1_option4 = raw_data$Post.Test.LT_problem1_option4,
+                        LT_problem1_option5 = raw_data$Post.Test.LT_problem1_option5,
+                        LT_problem1_option6 = raw_data$Post.Test.LT_problem1_option6,
+                        LT_problem2_option1 = raw_data$Post.Test.LT_problem2_option1,
+                        LT_problem2_option2 = raw_data$Post.Test.LT_problem2_option2,
+                        LT_problem2_option3 = raw_data$Post.Test.LT_problem2_option3,
+                        LT_problem2_option4 = raw_data$Post.Test.LT_problem2_option4,
+                        LT_problem2_option5 = raw_data$Post.Test.LT_problem2_option5,
+                        LT_problem2_option6 = raw_data$Post.Test.LT_problem2_option6,
+                        eq_problem1_box     = raw_data$Post.Test.eq_problem1_box,
+                        eq_problem2_box     = raw_data$Post.Test.eq_problem2_box,
+                        eq_problem3_box     = raw_data$Post.Test.eq_problem3_box,
+                        eq_problem4_box     = raw_data$Post.Test.eq_problem4_box,
+                        eq_problem5_box     = raw_data$Post.Test.eq_problem5_box,
+                        eq_problem6_box     = raw_data$Post.Test.eq_problem6_box,
+                        eq_problem7_box     = raw_data$Post.Test.eq_problem7_box)
